8c6490b975c63f7eb65bffb934aaeaae407f6733
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2025-10-15 09:18:06 +0900

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,21 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "rose-pine/neovim",
+    enabled = false,
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+  {
     "rebelot/kanagawa.nvim",
+    enabled = false,
     name = "kanagawa",
     priority = 1000,
     config = function()
